d9386cae55e740f962d0baeefab022cff0359f49
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-10-18 16:31:11 +0800

    Update RescheduleCommand to implement Undo

---
+++ src/main/java/taskle/logic/commands/UndoRescheduleCommand.java
@@ -0,0 +1,37 @@
+package taskle.logic.commands;
+
+import taskle.model.Model;
+import taskle.model.task.Task;
+import taskle.model.task.UniqueTaskList;
+import taskle.model.task.UniqueTaskList.DuplicateTaskException;
+import taskle.model.task.UniqueTaskList.TaskNotFoundException;
+
+/**
+ * UndoRescheduleCommand to handle undo of reschedule commands
+ * @author Muhammad Hamsyari
+ *
+ */
+public class UndoRescheduleCommand extends UndoCommand {
+
+    public UndoRescheduleCommand() {}
+
+    public CommandResult undoReschedule(Command command, Model model) {
+        assert command != null;
+        
+        Task task = command.getTasksAffected().get(0);
+        RescheduleCommand rescheduleCommand = (RescheduleCommand) command;
+        try {
+            System.out.println("After Index: " + (rescheduleCommand.targetIndex - 1));
+            model.editTaskDate(rescheduleCommand.targetIndex - 1, rescheduleCommand.dates);
+        } catch (TaskNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new CommandResult(
+                String.format(MESSAGE_SUCCESS, command.getCommandWord(), command.getTasksAffected().get(0).toString()));
+    }
+    
+    @Override
+    public String getCommandWord() {
+        return RescheduleCommand.COMMAND_WORD;
+    }
+}
