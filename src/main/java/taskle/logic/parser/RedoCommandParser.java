17ef87dbf2980333cde502d3536f18922428e134
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-10-21 22:45:49 +0800

    Add comments to RedoCommandParser class

--- src/main/java/taskle/logic/parser/RedoCommandParser.java
+++ src/main/java/taskle/logic/parser/RedoCommandParser.java
@@ -3,6 +3,11 @@ package taskle.logic.parser;
 import taskle.logic.commands.Command;
 import taskle.logic.commands.RedoCommand;
 
+/**
+ * RedoCommandParser class to handle parsing of redo commands
+ * @author Muhammad Hamsyari
+ *
+ */
 public class RedoCommandParser extends CommandParser {
 
     public RedoCommandParser() {
