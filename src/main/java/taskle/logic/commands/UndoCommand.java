a076a93cbc6ecb671e36ea0b3d8e20ec0c0d9707
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-10-21 22:07:50 +0800

    Implement refined undo command

--- src/main/java/taskle/logic/commands/UndoCommand.java
+++ src/main/java/taskle/logic/commands/UndoCommand.java
@@ -1,18 +1,5 @@
 package taskle.logic.commands;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import taskle.logic.commands.undo.UndoAddCommand;
-import taskle.logic.commands.undo.UndoClearCommand;
-import taskle.logic.commands.undo.UndoDoneCommand;
-import taskle.logic.commands.undo.UndoEditCommand;
-import taskle.logic.commands.undo.UndoRemoveCommand;
-import taskle.logic.commands.undo.UndoRescheduleCommand;
-import taskle.logic.history.History;
-import taskle.model.Model;
-
 /**
  * Undo recent command entered.
  */
@@ -23,7 +10,9 @@ public class UndoCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undo most recent command." + "Example: "
             + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Restored previous command: [%s %s]";
+    public static final String MESSAGE_SUCCESS = "Restored previous command.";
+    
+    public static final String MESSAGE_EMPTY_HISTORY = "Empty History. Nothing to Undo.";
     
     public UndoCommand() {
         
@@ -31,7 +20,10 @@ public class UndoCommand extends Command {
     
     @Override
     public CommandResult execute() {
-        return null;
+        if (!model.restoreTaskManager()) {
+            return new CommandResult(MESSAGE_EMPTY_HISTORY);
+        }
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 
     @Override
