3070d61d4e39d74a592175617b4e851181cede5b
abellimz <abellim1309@gmail.com>
2016-10-25 00:33:32 +0800

    Fix reschedule not sorted bug

--- src/main/java/taskle/model/task/TaskList.java
+++ src/main/java/taskle/model/task/TaskList.java
@@ -125,6 +125,7 @@ public class TaskList implements Iterable<Task> {
         } else {
             logger.severe("Number of dates is either 0 or exceed 2. Unable to update.");
         }
+        refreshInternalList();
     }
     
     public void refreshInternalList() {
