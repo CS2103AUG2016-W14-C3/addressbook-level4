0aec16e4f3543cfdd83599020c9477ec278fadf2
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-10-24 21:23:13 +0800

    Add tests for updateDirectory

--- src/test/java/taskle/commons/util/StorageDirectoryUtilTest.java
+++ src/test/java/taskle/commons/util/StorageDirectoryUtilTest.java
@@ -1,9 +1,13 @@
 package taskle.commons.util;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
 import java.util.logging.Level;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
@@ -19,12 +23,28 @@ public class StorageDirectoryUtilTest {
     private Model model;
     private Logic logic;
     private Config config;
-
+    
+    private static final String TEST_DATA_FOLDER = FileUtil.getPath("src/test/data/StorageDirectoryUtilTest/");
+    
     @Rule
     public ExpectedException thrown = ExpectedException.none();
     
     @Rule
     public TemporaryFolder saveFolder = new TemporaryFolder();
+    
+    //Change to a null directory - Shows Assertion Error
+    @Test
+    public void updateDirectory_nullDirectory_assertionError() {
+        thrown.expect(AssertionError.class);
+        StorageDirectoryUtil.updateDirectory(getTypicalConfig(), logic, null);
+    }
+    
+    //Change to a valid directory - Successfully Update
+    @Test
+    public void updateDirectory_validDirectory_directoryChanged() {
+        StorageDirectoryUtil.updateDirectory(config, logic, new File(TEST_DATA_FOLDER));
+        assertTrue(config.getTaskManagerFileDirectory().contains(TEST_DATA_FOLDER.substring(0, TEST_DATA_FOLDER.length() - 1)));
+    }
     
     @Before
     public void setup() {
