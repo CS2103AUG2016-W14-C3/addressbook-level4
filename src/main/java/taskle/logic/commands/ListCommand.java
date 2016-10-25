d6e53bb9146ed0817de6ce0aadf9186c0940600c
abellimz <abellim1309@gmail.com>
2016-10-25 09:02:26 +0800

    Revert "Delete unused ListCommand class"
    This reverts commit b43556d1c1e731d9d7045f98c8e6f1dbb62e671f.

---
+++ src/main/java/taskle/logic/commands/ListCommand.java
@@ -0,0 +1,25 @@
+package taskle.logic.commands;
+
+
+/**
+ * Lists all tasks in the Task Manager to the user.
+ */
+public class ListCommand extends Command {
+
+    public static final String COMMAND_WORD = "list";
+
+    public static final String MESSAGE_SUCCESS = "Listed all tasks";
+
+    public ListCommand() {}
+
+    @Override
+    public CommandResult execute() {
+        model.updateFilteredListToShowAllNotDone();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+    
+    @Override
+    public String getCommandWord() {
+        return COMMAND_WORD;
+    }
+}
