a414f38a81087983185056e5ad47490a5bb3f9b2
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-01 10:04:49 +0800

    Add checks for valid file path in OpenFileCommandParser

--- src/main/java/taskle/logic/parser/OpenFileCommandParser.java
+++ src/main/java/taskle/logic/parser/OpenFileCommandParser.java
@@ -1,9 +1,15 @@
 package taskle.logic.parser;
 
+import static taskle.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.io.File;
+
 import taskle.commons.exceptions.IllegalValueException;
+import taskle.commons.util.FileUtil;
 import taskle.logic.commands.Command;
 import taskle.logic.commands.IncorrectCommand;
 import taskle.logic.commands.OpenFileCommand;
+import taskle.logic.commands.RemoveCommand;
 
 /**
  * OpenFileParser class to handle parsing of open file commands
@@ -17,14 +23,16 @@ public class OpenFileCommandParser extends CommandParser {
 
     @Override
     public Command parseCommand(String args) {
-        return parseOpenFile(args);
+        return parseOpenFile(args.trim());
     }
     
     private Command parseOpenFile(String filePath) {
-        try {
+        if (FileUtil.isFileExists(new File(filePath))) {
             return new OpenFileCommand(filePath);
-        } catch (IllegalValueException ive) {
-            return new IncorrectCommand(ive.getMessage());
+        } else {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, 
+                                  OpenFileCommand.MESSAGE_USAGE));
         }
     }
 
