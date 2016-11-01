4c9c8eca1c07607d58cbf512a54e8bec592c3433
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-01 11:59:58 +0800

    Add test to change to a valid directory

--- src/test/java/guitests/ChangeDirectoryCommandTest.java
+++ src/test/java/guitests/ChangeDirectoryCommandTest.java
@@ -1,11 +1,16 @@
 package guitests;
 
+import static org.junit.Assert.assertTrue;
 import static taskle.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
 
+import taskle.commons.core.Config;
+import taskle.commons.exceptions.DataConversionException;
+import taskle.commons.util.ConfigUtil;
 import taskle.logic.commands.ChangeDirectoryCommand;
 
 //Tests for change in directory
@@ -19,6 +24,20 @@ public class ChangeDirectoryCommandTest extends TaskManagerGuiTest {
     public void changeDirectory_invalidDirectory_incorrectCommand() {
         String command = ChangeDirectoryCommand.COMMAND_WORD + " /i/n/v/a/l/i/d";
         assertChangeDirectoryInvalidDirectory(command);
+    }
+    
+    //Change to a valid directory
+    @Test
+    public void changeDirectory_validFormat_directoryChanged() throws DataConversionException, IOException {
+        String command = ChangeDirectoryCommand.COMMAND_WORD + " " + TEST_DATA_FOLDER;
+        assertChangeDirectorySuccess(command);
+    }
+    
+    private void assertChangeDirectorySuccess(String command) throws DataConversionException, IOException {
+        commandBox.runCommand(command);
+        Config config = ConfigUtil.readConfig(Config.DEFAULT_CONFIG_FILE).get();
+        config = ConfigUtil.readConfig(Config.DEFAULT_CONFIG_FILE).get();
+        assertTrue(config.getTaskManagerFileDirectory().contains(TEST_DATA_FOLDER.substring(0, TEST_DATA_FOLDER.length() - 1)));
     }
     
     private void assertChangeDirectoryInvalidDirectory(String command) {
