f556da6968d7e5b7afed8292ab31fbd9f124bf7e
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-01 12:22:45 +0800

    Add test for opening inexistent file

--- src/test/java/guitests/OpenFileCommandTest.java
+++ src/test/java/guitests/OpenFileCommandTest.java
@@ -1,6 +1,12 @@
 package guitests;
 
+import static taskle.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import java.io.File;
+
+import org.junit.Test;
+
+import taskle.logic.commands.OpenFileCommand;
 
 //Test for opening a new file
 public class OpenFileCommandTest extends TaskManagerGuiTest {
@@ -11,5 +17,17 @@ public class OpenFileCommandTest extends TaskManagerGuiTest {
     private static final String INEXISTENT_FILE = " Inexistent.xml";
     private static final String INVALID_FILE = "InvalidFormatTaskManager.xml";
     private static final String VALID_FILE = "ValidFormatTaskManager.xml";
+    
+    //Open an inexistent file
+    @Test
+    public void openFile_inexistentFile_incorrectCommand() {
+        String command = OpenFileCommand.COMMAND_WORD + INEXISTENT_FILE;
+        assertOpenFileInexistentFile(command);
+    }
+    
+    private void assertOpenFileInexistentFile(String command) {
+        commandBox.runCommand(command);
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, OpenFileCommand.MESSAGE_USAGE));
+    }
     
 }
