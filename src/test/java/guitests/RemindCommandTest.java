32ad7b8e6ddb5f019796270adbd5b987f3241b29
Zhi Yong <zhiyong.ong@hotmail.com>
2016-11-02 03:33:16 +0800

    Change tests name

--- src/test/java/guitests/RemindCommandTest.java
+++ src/test/java/guitests/RemindCommandTest.java
@@ -27,7 +27,7 @@ public class RemindCommandTest extends TaskManagerGuiTest {
      * @throws IllegalValueException
      */
     @Test
-    public void remind_task_success() throws IllegalValueException {
+    public void setReminder_normalDateSet_success() throws IllegalValueException {
         String index = "3";
         TaskCardHandle oldTask = taskListPanel.getTaskCardHandle(Integer.parseInt(index) - 1);
         String name = oldTask.getFullName();
@@ -43,11 +43,35 @@ public class RemindCommandTest extends TaskManagerGuiTest {
     }
 
     /**
+     * Set a reminder for a current task inside the TypicalTestTask to
+     * test the remind function. Check if that task has been edited
+     * correctly.
+     * 
+     * @throws IllegalValueException
+     */
+    @Test
+    public void setReminder_reminderPastEndDate_failure() throws IllegalValueException {
+        String index = "1";
+        TaskCardHandle oldTask = taskListPanel.getTaskCardHandle(Integer.parseInt(index) - 1);
+        String name = oldTask.getFullName();
+        String newRemindDateString = "13 Dec 7pm";
+        Date date = DateParser.parse(newRemindDateString).get(0);
+        String formattedNewDate = DateFormatUtil.formatDate(date);
+        String oldRemindDate = oldTask.getRemindDetails();
+        assertRemindResultSuccess("remind " + index + " " + newRemindDateString, 
+                name + " " + oldRemindDate + " -> " + formattedNewDate);
+        TaskCardHandle addedCard = taskListPanel.getTaskCardHandle(Integer.parseInt(index) - 1);
+        FloatTask newTask = new FloatTask(new Name(name), date, new UniqueTagList());
+        assertMatching(newTask, addedCard);
+    }
+
+    
+    /**
      * Set a reminder with no specified time to a task
      * @throws IllegalValueException
      */
     @Test
-    public void remind_taskNoInputTime_success() throws IllegalValueException {
+    public void setReminder_taskNoInputTime_success() throws IllegalValueException {
         String index = "3";
         TaskCardHandle oldTask = taskListPanel.getTaskCardHandle(Integer.parseInt(index) - 1);
         String name = oldTask.getFullName();
@@ -66,33 +90,33 @@ public class RemindCommandTest extends TaskManagerGuiTest {
      * Set reminder to an inexistent task
      */
     @Test
-    public void remind_inexistentTask_failure() {
+    public void setReminder_inexistentTask_failure() {
         String commandInvalidIntegerIndex = buildCommand("10", "31 Oct 10pm");
-        assertRescheduleInvalidIndex(commandInvalidIntegerIndex);
+        assertRemindInvalidIndex(commandInvalidIntegerIndex);
 
         String commandInvalidStringIndex = buildCommand("ABC", "31 Oct 10pm");
-        assertRescheduleInvalidCommandFormat(commandInvalidStringIndex);
+        assertRemindInvalidCommandFormat(commandInvalidStringIndex);
 
         String commandInvalidNegativeIntegerIndex = buildCommand("-1", "31 Oct 10pm");
-        assertRescheduleInvalidCommandFormat(commandInvalidNegativeIntegerIndex);
+        assertRemindInvalidCommandFormat(commandInvalidNegativeIntegerIndex);
     }
 
     /**
      * Set a reminder with more than 1 date
      */
     @Test
-    public void remind_moreThan1Date_failure() {
+    public void setReminder_moreThan1Date_failure() {
         String commandTooManyDates = buildCommand("1", "31 Oct 10pm to 1 Nov 11pm");
-        assertRescheduleInvalidCommandFormat(commandTooManyDates);
+        assertRemindInvalidCommandFormat(commandTooManyDates);
     }
 
     /**
      * Set a reminder with an invalid date
      */
     @Test
-    public void remind_taskInvalidDate_failure() {
+    public void setReminder_taskInvalidDate_failure() {
         String commandNoDates = buildCommand("1", "no date");
-        assertRescheduleInvalidCommandFormat(commandNoDates);
+        assertRemindInvalidCommandFormat(commandNoDates);
     }
     
     private String buildCommand(String taskNumber, String date) {
@@ -105,14 +129,19 @@ public class RemindCommandTest extends TaskManagerGuiTest {
         assertSuccessfulMessage("Set Reminder Date: " + newName);
     }
 
-    private void assertRescheduleInvalidIndex(String command) {
+    private void assertRemindInvalidIndex(String command) {
         commandBox.runCommand(command);
         assertUnsuccessfulMessage(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
     }
 
-    private void assertRescheduleInvalidCommandFormat(String command) {
+    private void assertRemindInvalidCommandFormat(String command) {
         commandBox.runCommand(command);
         assertUnsuccessfulMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemindCommand.MESSAGE_USAGE));
+    }
+    
+    private void assertRemindPastEndDate(String command) {
+        commandBox.runCommand(command);
+        assertUnsuccessfulMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, Messages.));
     }
 
 }
