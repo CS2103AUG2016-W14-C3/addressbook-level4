cb8ab59afaacca50ec3ec407a214bc0998004c10
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-01 10:32:09 +0800

    Add ChangeDirectoryCommand

---
+++ src/main/java/taskle/logic/commands/ChangeDirectoryCommand.java
@@ -0,0 +1,41 @@
+package taskle.logic.commands;
+
+import java.io.File;
+
+import taskle.commons.util.StorageDirectoryUtil;
+
+//Change directory of current storage file
+public class ChangeDirectoryCommand extends Command {
+
+    public static final String COMMAND_WORD = "changedirectory";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Reads data from specified file.\n"
+            + "Format: add directory\n"
+            + "Example: " + COMMAND_WORD + " C:" + File.separator + "Users" + File.separator + "John"
+            + File.separator + "desktop";
+
+    public static final String MESSAGE_SUCCESS = "Storage Directory has been changed to %1$s";
+    
+    public static final String MESSAGE_FAILURE = "An error occurred when changing directory.";
+    
+    private final File file;
+    
+    public ChangeDirectoryCommand(String directoryPath) {
+        file = new File(directoryPath);
+    }
+    
+    @Override
+    public CommandResult execute() {
+        if (StorageDirectoryUtil.updateDirectory(file)) {
+            return new CommandResult(String.format(MESSAGE_SUCCESS, file.getAbsolutePath()));
+        } else {
+            return new CommandResult(MESSAGE_FAILURE);
+        }
+    }
+
+    @Override
+    public String getCommandWord() {
+        return COMMAND_WORD;
+    }
+
+}
