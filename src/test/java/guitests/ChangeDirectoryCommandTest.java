bec95fd7c0b84ab3362ba0095f6ff08575eba8cd
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-01 11:58:32 +0800

    Add ChangeDirectoryCommandTest

---
+++ src/test/java/guitests/ChangeDirectoryCommandTest.java
@@ -0,0 +1,12 @@
+package guitests;
+
+import java.io.File;
+
+//Tests for change in directory
+public class ChangeDirectoryCommandTest extends TaskManagerGuiTest {
+    
+    private static final String TEST_DATA_FOLDER = "src" + File.separator + "test" +
+            File.separator + "data" + File.separator + "StorageDirectoryUtilTest";
+    
+    
+}
