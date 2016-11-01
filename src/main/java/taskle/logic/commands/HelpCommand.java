dc01cf20a3e561be0022c17bd57a1e45f2028b48
DESKTOP-CO22UMA\silfe <silfer.taintedcross@outlook.com>
2016-11-02 02:07:06 +0800

    Edited Specific Information (Help and Exit Commands)

--- src/main/java/taskle/logic/commands/HelpCommand.java
+++ src/main/java/taskle/logic/commands/HelpCommand.java
@@ -11,10 +11,10 @@ public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays all the commands in Taskle!\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_MESSAGE = "The Help Window is Opened.";
 
     public HelpCommand() {}
 
