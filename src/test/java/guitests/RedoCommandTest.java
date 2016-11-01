0091d2f32cb3d79835d61ae2bccde7a85b45810f
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-01 21:30:42 +0800

    Add setUp and tearDown methods in RedoCommandTest

--- src/test/java/guitests/RedoCommandTest.java
+++ src/test/java/guitests/RedoCommandTest.java
@@ -2,9 +2,22 @@ package guitests;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import taskle.commons.core.Config;
+import taskle.commons.exceptions.DataConversionException;
+import taskle.commons.util.ConfigUtil;
+import taskle.commons.util.FileUtil;
 import taskle.logic.commands.AddCommand;
+import taskle.logic.commands.ChangeDirectoryCommand;
+import taskle.logic.commands.OpenFileCommand;
 import taskle.logic.commands.RedoCommand;
 import taskle.logic.commands.RemoveCommand;
 import taskle.logic.commands.UndoCommand;
@@ -13,6 +26,14 @@ import taskle.testutil.TestUtil;
 
 //@@author A0140047U
 public class RedoCommandTest extends TaskManagerGuiTest {
+    
+    private static String TEST_DATA_FOLDER = FileUtil.getPath("./src/test/data/StorageDirectoryUtilTest/");
+    private static String TEST_DATA_FILE = TEST_DATA_FOLDER + "ValidFormatTaskManager.xml";
+    
+    private Config config;
+    private String taskManagerDirectory;
+    private String taskManagerFileName;
+    
     @Test
     public void redo() {
         Task[] currentList = td.getTypicalTasks();
@@ -30,13 +51,22 @@ public class RedoCommandTest extends TaskManagerGuiTest {
         commandBox.runCommand(RemoveCommand.COMMAND_WORD + " 1");
         currentList = TestUtil.removeTaskFromList(currentList, 1);
         assertRedoSuccess(RedoCommand.MESSAGE_NOTHING_TO_REDO, currentList);
+        
+
+    
+    //Stores original taskManager directory and file name
+    @Before
+    public void setUp() throws DataConversionException {
+        config = ConfigUtil.readConfig(Config.DEFAULT_CONFIG_FILE).get();
+        taskManagerDirectory = config.getTaskManagerFileDirectory();
+        taskManagerFileName = config.getTaskManagerFileName();
     }
     
-    private void assertRedoSuccess(String message, Task... expectedHits) {
-        commandBox.runCommand(RedoCommand.COMMAND_WORD);
-
-        assertListSize(expectedHits.length);
-        assertTrue(taskListPanel.isListMatching(expectedHits.length));
-        assertSuccessfulMessage(message);
+    //Restores original taskManager directory and file name
+    @After
+    public void tearDown() throws IOException {
+        config.setTaskManagerFileDirectory(taskManagerDirectory);
+        config.setTaskManagerFileName(taskManagerFileName);
+        ConfigUtil.saveConfig(config, Config.DEFAULT_CONFIG_FILE);
     }
 }
