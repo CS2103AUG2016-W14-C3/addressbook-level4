643cbf43384901c7728161bc2278e575a8149a6f
Zhi Yong <zhiyong.ong@hotmail.com>
2016-11-04 00:36:33 +0800

    Removed unDone command

--- src/main/java/taskle/model/task/TaskList.java
+++ src/main/java/taskle/model/task/TaskList.java
@@ -15,7 +15,6 @@ import taskle.commons.core.Messages;
 import taskle.commons.util.CollectionUtil;
 import taskle.commons.util.TaskUtil;
 import taskle.ui.CommandBox;
-import taskle.ui.SystemTray;
 
 /**
  * A list of tasks that does not allow nulls.
@@ -88,14 +87,6 @@ public class TaskList implements Iterable<Task> {
         logger.info("Task " + index + " Done! ");
         refreshInternalList();
     }
-    // @@author
-
-    public void unDone(Task taskToUndo) {
-        int targetIndex = internalList.indexOf(taskToUndo);
-        taskToUndo.setTaskDone(false);
-        internalList.set(targetIndex, taskToUndo);
-        refreshInternalList();
-    }
 
     // @@author A0139402M
     /**
@@ -159,8 +150,10 @@ public class TaskList implements Iterable<Task> {
         }
         Task toEdit = toEditOp.get();
         
-        if(checkInvalidRemindDate(toEdit, date)) {
-            return Messages.MESSAGE_REMINDER_AFTER_FINAL_DATE;
+        if(date != null) {
+            if(checkInvalidRemindDate(toEdit, date)) {
+                return Messages.MESSAGE_REMINDER_AFTER_FINAL_DATE;
+            }
         }
         toEdit.setRemindDate(date);
         internalList.set(index, toEdit);
