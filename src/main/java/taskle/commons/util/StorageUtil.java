042a35acdfa331018a441242d62bf42176b36277
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-04 09:31:31 +0800

    Add enum OperationType to better handle storage operations

--- src/main/java/taskle/commons/util/StorageUtil.java
+++ src/main/java/taskle/commons/util/StorageUtil.java
@@ -17,10 +17,16 @@ public class StorageUtil {
     
     private static Stack<Config> configHistory = new Stack<Config>();
     private static Stack<Config> redoConfigHistory = new Stack<Config>();
+    private static Stack<OperationType> operationHistory = new Stack<OperationType>();
+    private static Stack<OperationType> redoOperationHistory = new Stack<OperationType>();
     
     private static final int INDEX_DIRECTORY = 0;
     private static final int INDEX_FILE_NAME = 1;
     private static final int FILE_PATH_ARRAY_LENGTH = 2;
+
+    public enum OperationType {
+        CHANGE_DIRECTORY, OPEN_FILE
+    }
     
     /**
      * Moves file to the selected directory and updates Config accordingly
