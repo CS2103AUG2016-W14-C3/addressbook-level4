d28b8bf7bf713b275cfceb752de52c4bfaec32ea
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-05 19:45:58 +0800

    Add Message for Config Error

--- src/main/java/taskle/commons/core/Messages.java
+++ src/main/java/taskle/commons/core/Messages.java
@@ -9,6 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d task listed!";
+    public static final String MESSAGE_CONFIG_ERROR = "Issues encountered when reading config file.\nPlease restart Taskle.";
     public static final String MESSAGE_REMINDER_AFTER_FINAL_DATE = "Reminder date cannot be dated after the end date of the task.";
 
 }
