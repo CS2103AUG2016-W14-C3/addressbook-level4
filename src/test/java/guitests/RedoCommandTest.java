6ad5c316a70dc71689973ebcb7c66e9f872e9619
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-05 20:24:36 +0800

    Add test for redo while invalid config

--- src/test/java/guitests/RedoCommandTest.java
+++ src/test/java/guitests/RedoCommandTest.java
@@ -2,6 +2,7 @@ package guitests;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.After;
@@ -10,9 +11,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import taskle.commons.core.Config;
+import taskle.commons.core.Messages;
 import taskle.commons.exceptions.DataConversionException;
 import taskle.commons.util.ConfigUtil;
 import taskle.commons.util.FileUtil;
+import taskle.commons.util.StorageUtil;
 import taskle.logic.commands.AddCommand;
 import taskle.logic.commands.ChangeDirectoryCommand;
 import taskle.logic.commands.OpenFileCommand;
@@ -29,6 +32,8 @@ public class RedoCommandTest extends TaskManagerGuiTest {
     private static String TEST_DATA_FOLDER_TEMP = FileUtil.getPath("src/test/data/StorageDirectoryUtilTest/temp");
     private static String TEST_DATA_FILE_NAME = "ValidFormatTaskManager.xml";
     private static String TEST_DATA_FILE = TEST_DATA_FOLDER + TEST_DATA_FILE_NAME;
+    private static final String INVALID_CONFIG = FileUtil.getPath("src/test/data/ConfigUtilTest/NotJasonFormatConfig.json");
+    private static final String TEMP_CONFIG = "temp.json";
     
     private Config config;
     private String taskManagerDirectory;
@@ -37,8 +42,9 @@ public class RedoCommandTest extends TaskManagerGuiTest {
     //Redo when no action has been undone
     @Test
     public void redo_emptyHistory_messageDisplayed() {
-        Task[] currentList = td.getTypicalTasks();
-        assertRedoSuccess(RedoCommand.MESSAGE_NOTHING_TO_REDO, currentList);
+        StorageUtil.clearHistory();
+        commandBox.runCommand(RedoCommand.COMMAND_WORD);
+        assertUnsuccessfulMessage(RedoCommand.MESSAGE_NOTHING_TO_REDO);
     }
     
     //Redo after undo of mutating command    
@@ -57,7 +63,8 @@ public class RedoCommandTest extends TaskManagerGuiTest {
         Task[] currentList = td.getTypicalTasks();
         commandBox.runCommand(RemoveCommand.COMMAND_WORD + " 1");
         currentList = TestUtil.removeTaskFromList(currentList, 1);
-        assertRedoSuccess(RedoCommand.MESSAGE_NOTHING_TO_REDO, currentList);
+        commandBox.runCommand(RedoCommand.COMMAND_WORD);
+        assertUnsuccessfulMessage(RedoCommand.MESSAGE_NOTHING_TO_REDO);
     }
     
     //Redo after undo of storage directory change
@@ -83,6 +90,22 @@ public class RedoCommandTest extends TaskManagerGuiTest {
         } catch (DataConversionException e) {
             e.printStackTrace();
         }
+    }
+    
+    //Redo storage operation when config file is invalid
+    @Test
+    public void redo_invalidConfig_dataConversionException() {
+        commandBox.runCommand(OpenFileCommand.COMMAND_WORD + " " + TEST_DATA_FILE);
+        commandBox.runCommand(UndoCommand.COMMAND_WORD);
+        
+        new File(Config.DEFAULT_CONFIG_FILE).renameTo(new File(TEMP_CONFIG));
+        new File(INVALID_CONFIG).renameTo(new File(Config.DEFAULT_CONFIG_FILE));
+        
+        commandBox.runCommand(RedoCommand.COMMAND_WORD);
+        assertUnsuccessfulMessage(Messages.MESSAGE_CONFIG_ERROR);
+        
+        new File(Config.DEFAULT_CONFIG_FILE).renameTo(new File(INVALID_CONFIG));
+        new File(TEMP_CONFIG).renameTo(new File(Config.DEFAULT_CONFIG_FILE));
     }
     
     private void assertRedoSuccess(String message, Task... expectedHits) {
