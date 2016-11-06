4e1b062620443273b11da124a92ab6f711c20d25
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-06 20:01:00 +0800

    Change unnecessary multiline comment to single line comment in taskle/testutil

--- src/test/java/taskle/testutil/EventsCollector.java
+++ src/test/java/taskle/testutil/EventsCollector.java
@@ -8,9 +8,7 @@ import taskle.commons.events.BaseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A class that collects events raised by other classes.
- */
+// A class that collects events raised by other classes.
 public class EventsCollector{
     List<BaseEvent> events = new ArrayList<BaseEvent>();
 
@@ -18,24 +16,18 @@ public class EventsCollector{
         EventsCenter.getInstance().registerHandler(this);
     }
 
-    /**
-     * Collects any event raised by any class
-     */
+    // Collects any event raised by any class
     @Subscribe
     public void collectEvent(BaseEvent event){
         events.add(event);
     }
 
-    /**
-     * Removes collected events from the collected list
-     */
+    // Removes collected events from the collected list
     public void reset(){
         events.clear();
     }
 
-    /**
-     * Returns the event at the specified index
-     */
+    // Returns the event at the specified index
     public BaseEvent get(int index){
         return events.get(index);
     }
