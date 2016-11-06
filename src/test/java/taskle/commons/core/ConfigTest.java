a747862040ed1243946d050b6a30628d38d5fbc4
Muhammad Hamsyari <hamsyari22@gmail.com>
2016-11-06 20:18:45 +0800

    Remove unused imports

--- src/test/java/taskle/commons/core/ConfigTest.java
+++ src/test/java/taskle/commons/core/ConfigTest.java
@@ -10,8 +10,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-
 public class ConfigTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
